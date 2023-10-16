8edb7f20afef23bd14e50917c4d7fb019db9c6b4
Nikita <terencekity@gmail.com>
2023-10-16 15:29:30 -0400

    add render cards

--- pizza-library/src/App.js
+++ pizza-library/src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import Header from './Header';
 import PizzaContainer from './PizzaContainer';
 import PizzaForm from './PizzaForm';
+import React,{useState,useEffect} from 'react';
 
 function App() {
+  const [pizzaObj,setPizza] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+      .then((r) => r.json())
+      .then((pizzasArr) => {
+        setPizza(pizzasArr)
+      });
+  }, []);
   return (
+
+
     <>
         <Header />
         <PizzaForm />
-        <PizzaContainer />
+        <PizzaContainer pizzaObj={pizzaObj}/>
     </>
   );
 }
--- pizza-library/src/PizzaContainer.js
+++ pizza-library/src/PizzaContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PizzaCard from "./PizzaCard";
 
-function PizzaContainer () {
+function PizzaContainer ({pizzaObj}) {
+    // console.log(pizzaObj)
+const renderedPizza = pizzaObj.map((el)=>{return (<PizzaCard key={el.id} el={el} />)})
     return (
-        <>
+        <ul className="cards">
             <p>Hello from the Pizza Container</p>
-            <PizzaCard />
-        </>
+            {renderedPizza}
+        </ul>
     )
 }
 
