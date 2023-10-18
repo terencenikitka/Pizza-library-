dd092def501691bdfbbc4280e6f8db8f91659831
Nikita <terencekity@gmail.com>
2023-10-18 15:29:02 -0400

    add some features

--- pizza-library/src/PizzaCard.js
+++ pizza-library/src/PizzaCard.js
@@ -5,25 +5,34 @@ function PizzaCard ({el}) {
     const renderedIngredient =  ingredient.map((el)=><li>{el}</li>)
 
     const [details,setDetails] = useState(false)
+    const [fav,setFav] = useState(false)
 
     function handleClick(){
       setDetails((cur)=>!cur)
+    }
+    function handleFav(){
+      setFav((cur)=>!cur)
     }
     
     return (
         <li className="cards__item">
             <div className="card">
             <h4 className="pizzaname">{name}</h4>
+         
             <img
           src={img}
           alt={name}
           className="card__image"
           onClick={handleClick}
-        />
+        />   
+        <div>
+     <button onClick={handleFav}>{fav?"🟊":'☆'}</button>
+     </div>
     <div>
       <h4 className="ingredientsname">Ingredients:</h4>
       <ul className="ingredientslist">{ details?renderedIngredient:''}</ul>
-    </div>
+    </div> 
+      
  </div>
  </li>   )
 }
--- pizza-library/src/PizzaContainer.js
+++ pizza-library/src/PizzaContainer.js
@@ -4,7 +4,7 @@ import { useOutletContext } from "react-router-dom";
 
 function PizzaContainer () {
     const {pizzaObj,onNewPizza} = useOutletContext()
-
+    
     const renderedPizza = onNewPizza(pizzaObj).map((el)=>{return (<PizzaCard  key={el.id} el={el} />)})
         return   (
             <div className="pizzacontainer">
